3a7bf64566bcad6cb18ef3cff0c128157694dfd9
pierrelouishance <pierrelouishance@gmail.com>
2024-02-23 15:48:21 +0100

    update database.py

--- app/database.py
+++ app/database.py
@@ -1,20 +1,21 @@
+from uuid import uuid4
 
 database = {
     "books": [
         {
-            "id": "1",
+            "id": str(uuid4()),
             "name": "le guide du voyageur intergalactique",
             "auteur": "Douglas Adams",
             "editeur": "Galliot",
         },
         {
-            "id": "2",
+            "id":  str(uuid4()),
             "name": "le dernier restaurant avant la fin du monde",
             "auteur": "Douglas Adams",
             "editeur": "De poche",
         },
         {
-            "id": "3",
+            "id":  str(uuid4()),
             "name": "La vie, l'univers et le reste",
             "auteur": "Douglas Adams",
             "editeur": "Flammarion",
