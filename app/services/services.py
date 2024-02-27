540e11612d752c9898d15fc7c409e277d5fefd95
pierrelouishance <pierrelouishance@gmail.com>
2024-02-27 22:05:17 +0100

    for merge

--- app/services/services.py
+++ app/services/services.py
@@ -14,6 +14,7 @@ def get_all_books()  -> list[Book]:
 
     books_data = database["books"]
     books = [Book.model_validate(data) for data in books_data] 
+    return books
 
 def save_book(new_book: Book) -> Book:
     """
